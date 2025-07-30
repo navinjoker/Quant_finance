ecadbcc8c8a1ddac4d4dc1a35b8023939743eb92
navinjoker <100150180+navinjoker@users.noreply.github.com>
2025-07-30 18:35:13 +0530

    Final Commit
    Final Commit

--- pipeline/predict.py
+++ pipeline/predict.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import load_model
 
 def predict_latest(X_seq, model_path):
-    model = load_model(model_path)
+    model = load_model(model_path, compile=False)
     return model.predict(X_seq)
